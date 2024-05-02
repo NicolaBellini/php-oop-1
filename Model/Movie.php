9a4d4a12c5b0abdd2aeaa1b94d95808668e3b1e2
NicolaBellini <bellininicola4366@gmail.com>
2024-05-02 15:38:42 +0200

    [add] multiple genres

--- Model/Movie.php
+++ Model/Movie.php
@@ -1,5 +1,5 @@
 <?php 
-
+require_once './db.php';
 class Movie{
 
   private $title;
@@ -7,7 +7,7 @@ class Movie{
   private $year;
   private $rating;
 
-  function __construct(string $_title, string $_genre,int $_year, float $_rating)
+  function __construct(string $_title, array $_genre,int $_year, float $_rating)
   {
     $this->title = $_title;
     $this->genre = $_genre;
@@ -16,7 +16,9 @@ class Movie{
   }
 
   public function getFullInfo(){
-    echo "TITOLO: $this->title,<br> GENERE: $this->genre,<br> ANNO: $this->year,<br> RATING: $this->rating ";
+    // ho imploso l' array in modo da poterlo leggere come stringa
+    $genres = implode(', ', $this->genre);
+    echo "TITOLO: $this->title,<br> GENERE: $genres,<br> ANNO: $this->year,<br> RATING: $this->rating ";
   }
 
 }
--- db.php
+++ db.php
@@ -4,25 +4,25 @@
 $films_data = [
   'baba' => [
     'title' => 'baba',
-    'genre' => 'drama',
+    'genre' => ['crime','action'],
     'year' => 1998,
     'rating' => 2.3
   ],
   'Mago_di_Oz' => [
     'title' => 'Mago Di Oz',
-    'genre' => 'Fantasy',
+    'genre' => ['Fantasy', 'action','drama'],
     'year' => 1967,
     'rating' => 3.4
   ],
   'Pulp_Fiction' => [
     'title' => 'Pulp Fiction',
-    'genre' => 'Crime',
+    'genre' => ['Crime'],
     'year' => 1994,
     'rating' => 4.5
   ],
   'Inception' => [
     'title' => 'Inception',
-    'genre' => 'Sci-Fi',
+    'genre' => ['Sci-Fi', 'thriller'],
     'year' => 2010,
     'rating' => 4.7
   ]
